add8cb8809103b960b02cc8aa48164f99dd7d22e
James Brown <jamesbrown5292@gmail.com>
2020-08-27 00:59:40 +0000

    linted

--- wordsearch.js
+++ wordsearch.js
@@ -1,35 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-    const backwardsJoin = letters.map(ls => ls.reverse().join(''));
-    for (l of backwardsJoin) {
-        if (l.includes(word)) return true
-    }
-    const transposedMatrix = [];
-    for (let i = 0; i < letters[0].length; i++) {
-        let tempArr = [];
-        for (let m = 0; m < letters.length; m++) {
-            tempArr.push(letters[m][i]);
-        }
-        transposedMatrix.push(tempArr);
-    };
-    const verticalJoin = transposedMatrix.map(ls => ls.join(''))
-    for (l of verticalJoin) {
-        if (l.includes(word)) return true
-    }
-    const verticalBack = transposedMatrix.map(ls => ls.reverse().join(''))
-    for (l of verticalBack) {
-        if (l.includes(word)) return true
-    }
-    return false;
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
   }
-  module.exports = wordSearch
+  const backwardsJoin = letters.map(ls => ls.reverse().join(''));
+  for (let l of backwardsJoin) {
+    if (l.includes(word)) return true;
+  }
+  const transposedMatrix = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let tempArr = [];
+    for (let m = 0; m < letters.length; m++) {
+      tempArr.push(letters[m][i]);
+    }
+    transposedMatrix.push(tempArr);
+  }
+  const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalBack = transposedMatrix.map(ls => ls.reverse().join(''));
+  for (let l of verticalBack) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
